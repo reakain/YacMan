1081b22e54a84ab49c67305ed94b8ab71e1ce587
Reakain <reakain@users.noreply.github.com>
2019-04-07 18:48:57 -0400

    Initial base game build

---
+++ Assets/Scripts/Pacdot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YacmanGame
+{
+    public class Pacdot : MonoBehaviour
+    {
+        void OnTriggerEnter2D(Collider2D co)
+        {
+            if (co.name == "Yacman")
+                Destroy(gameObject);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# YacMan
+Quick Unity YacMan game in Unity version 2018.3.0f2
+
+Developing to run entire Yacman game from single game object instantiation/deletion
+
+Base level game started following tutorial: https://noobtuts.com/unity/2d-pacman-game
